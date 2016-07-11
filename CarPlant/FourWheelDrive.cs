795a2d976378bf076a03064c130073ca978d3cff
Chris Ross <chris.ross@7digital.com>
2016-07-11 17:38:08 +0100

    Adds FourWheelDrive class (forgot to add)

---
+++ CarPlant/FourWheelDrive.cs
@@ -0,0 +1,20 @@
+namespace CarPlant
+{
+	class FourWheelDrive
+	{
+		private readonly Wheels _wheels;
+
+		public FourWheelDrive(Wheels wheels)
+		{
+			_wheels = wheels;
+		}
+
+		public void Accelerate(int kph)
+		{
+			_wheels.FrontLeft.rotate(kph);
+			_wheels.FrontRight.rotate(kph);
+			_wheels.RearLeft.rotate(kph);
+			_wheels.RearRight.rotate(kph);
+		}
+	}
+}
